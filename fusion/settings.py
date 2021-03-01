5cf989413442a26df0e2a04aca10e209dd65d8c4
Leo5677 <48198740+Leo5677@users.noreply.github.com>
2021-02-28 22:01:54 -0300

    Projetofinalizado

--- fusion/settings.py
+++ fusion/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6c84000n*5423ov#u9h6f@&+%1ph4rxi(t1!83z+1u@(%2hh!6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
